87aeb9a122c627041c1b3f0f25fbf9b63b26dca2
Ahmed henchiri <ahmedhenchiri00@gmail.com>
2023-02-07 11:42:02 +0100

    connect to mongodb

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
-const express = require("express")
+const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose")
+const app = express() ; 
 
-const app = express()
-app.use(express.json())
-
+app.use(express.json()) ; 
+app.use(cors()) ;
 const PORT = 6000 ; 
+const db ="mongodb://localhost:27017/"
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => {
+    console.log('your database is connect ..');
+  })
+  .catch((err) => console.log(err));
 
 app.listen(PORT,()=>{
     console.log(`your server listening in port ${PORT}`)
