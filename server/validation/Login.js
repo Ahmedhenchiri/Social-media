0e5d5a806ee85527db98f5281252cc6c5f316a95
Ahmed henchiri <ahmedhenchiri00@gmail.com>
2023-02-07 12:45:31 +0100

    create validation of login in backend

---
+++ server/validation/Login.js
@@ -0,0 +1,25 @@
+const isEmpty = require("./isEmpty")
+const validator = require ("validator")
+
+module.exports = function validateLogin(data){
+let error={}
+data.email= !isEmpty(data.email) ? data.email : ""
+data.password = !isEmpty (data.password) ? data.password : ""
+
+if (!validator.isEmail(data.email)){
+    error.email ="Required format email "
+   }
+   if (validator.isEmpty(data.email)){
+    error.email ="Required email"
+   }
+   if (validator.isEmpty(data.password)){
+    error.password ="Required password"
+   }
+ 
+  
+
+   return {
+    error,
+    isValid: isEmpty(error)
+   }
+} 
