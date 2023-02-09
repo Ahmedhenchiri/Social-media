bc74b4d3ecaddb1ff8b325373427dedeebefe858
Ahmed henchiri <ahmedhenchiri00@gmail.com>
2023-02-09 21:12:50 +0100

    fix the routes of the user table

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose")
+
+
 const app = express() ; 
 
 app.use(express.json()) ; 
 app.use(cors()) ;
+// require('dotenv').config()
 const PORT = 6000 ; 
-const db ="mongodb://localhost:27017/"
+const userRouter = require("./routes/user.router")
+app.use("/user",userRouter)
+
+
+
+const db ="mongodb://localhost:27017/social-media"
 mongoose.set('strictQuery', true);
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
