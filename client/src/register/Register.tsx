d1c4c57ad012f3348270aa2bf6b6e1e48c3bf399
Ahmed henchiri <ahmedhenchiri00@gmail.com>
2023-02-10 11:02:57 +0100

    fix some cs in componet register

---
+++ client/src/register/Register.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./register.css"
+function Register() {
+  return (
+    <div className='container'>
+   <div className='register'>Register</div>
+   </div>
+  )
+}
+
+export default Register
