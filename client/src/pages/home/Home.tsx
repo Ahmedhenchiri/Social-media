1f46c70d378b0a197d10da7aa62d63772e0ef7cc
Ahmed henchiri <ahmedhenchiri00@gmail.com>
2023-02-27 15:30:08 +0100

    getAll post from databases

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,12 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import api from '../../api/Api';
 import Modal from '../../componets/custom/Modal/Modal'
 
 const  Home = () => {
+ const [data,setData] = useState<any>([]) 
  
+ const getAllData = async () => {
+  try{
+  const response = await api.get("/post/")
+  console.log("🚀 ~ file: home.tsx:13 ~ getAllData ~ response:", response.data)
+  
+  setData(response.data)
+}catch(error){
+  console.log(error)
+}
+ }
+ useEffect(()=>{
+  getAllData()
+ },[])
+
   return (
       <div>
+        
       <Modal />
       </div>
   )
