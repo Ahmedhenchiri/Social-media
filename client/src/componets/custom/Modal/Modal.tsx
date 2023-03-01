c3023d25d19e6eb9d9d4a5d24c9c5ab80e076389
Ahmed henchiri <ahmedhenchiri00@gmail.com>
2023-03-01 10:04:07 +0100

    install Yup to validate inputs

--- client/src/componets/custom/Modal/Modal.tsx
+++ client/src/componets/custom/Modal/Modal.tsx
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import api from '../../../api/Api';
-import { Formik, Form, Field } from 'formik';
+
 
 const Modale = () => {
     const [show, setShow] = useState(false);
