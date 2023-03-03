c5828f8b415efd771173ff782dae53f35565d47b
Ahmed henchiri <ahmedhenchiri00@gmail.com>
2023-03-03 09:52:28 +0100

    add template of post

--- client/src/componets/Post/Post.tsx
+++ client/src/componets/Post/Post.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
 const Post = () => {
+    
   return (
-    <div>Post</div>
+    <div className="blog-list-item" >
+    <div className="blog-list-item-title">{data.title}</div>
+    <div className="blog-list-item-byline">
+      <span className="blog-list-item-byline-author"> {data.blog} </span>
+   
+    </div>
+   
+    <div className="blog-list-item-lede">
+      <img src={data.imageUrl} alt="no content" />
+     
+    </div>
+  </div>
   )
 }
 
