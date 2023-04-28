22e10498f47743ee82f9804730df211f3ceedab0
Ahmed henchiri <ahmedhenchiri00@gmail.com>
2023-04-28 14:47:34 +0100

    add file css to navabr

--- client/src/componets/navbar/Navbar.tsx
+++ client/src/componets/navbar/Navbar.tsx
@@ -20,25 +20,37 @@ const Navba = () => {
             <>
               <Nav.Link href="/">Home</Nav.Link>
              
-              <Nav.Link href="/profile">  <img src={image} alt="no image"  style={{ width: "35px", height: "35px", borderRadius: "50%" }}
-            />
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <Nav.Link href="/profile"> 
+               {/* <img src={image} alt="no image"  style={{ width: "35px", height: "35px", borderRadius: "50%" }}
+            /> */}
+            <NavDropdown id="basic-nav-dropdown" title={<img src={image} alt="no image" style={{ width: "35px", height: "35px", borderRadius: "50%" }} />}>
+              <NavDropdown.Item href="/profile">
+                    <img
+                      src={image}
+                      alt="no image"
+                      style={{ width: "35px", height: "35px", borderRadius: "50%" }}
+                    />
+                    {"  "}
+                    {Data.name}
+                  </NavDropdown.Item>
+             
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
               </NavDropdown.Item>
-              <NavDropdown.Item href="/login" onClick={logout}> Log Out <i className="fa-solid fa-right-to-bracket "></i></NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
+              {/* <NavDropdown.Item href="/login" onClick={logout}> Log Out <i className="fa-solid fa-right-to-bracket "></i></NavDropdown.Item>
+              <NavDropdown.Divider /> */}
+              <NavDropdown.Item href="/login" onClick={logout}>
+              Log Out <i className="fa-solid fa-right-to-bracket "></i>
+               
               </NavDropdown.Item>
             </NavDropdown>
             </Nav.Link>
               
-              <Nav.Link href="/login" onClick={logout}>
+              {/* <Nav.Link href="/login" onClick={logout}>
                 {" "}
                 Log Out <i className="fa-solid fa-right-to-bracket "></i>
-              </Nav.Link>
+              </Nav.Link> */}
             </>
           ) : (
             <>
